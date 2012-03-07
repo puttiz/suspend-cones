f56879e13441a3e7535c8205f69d6a77957b2629
puttiz <puttiz@gmail.com>
2012-03-07 23:18:34 +0800

    Copy & Paste HTML

---
+++ header.php
@@ -0,0 +1,30 @@
+  <div class="topbar bar dark"></div>
+  <!--
+    Masthead
+    ==========================================================================
+  -->
+  <div class="header container">
+    <div class="span2 offset1">
+      <h1 class="logo">Glimmer</h1>
+      <p class="lead"></p>
+    </div>
+    <div class="mastnav span8">
+      <ul class="nav">
+        <li>
+          <a class="active" href="./home.php">home</a>
+        </li>
+        <li>
+          <a href="./shop.php">shop</a>
+        </li>
+        <li>
+          <a href="./collections.php">collections</a>
+        </li>
+        <li>
+          <a href="./blog">blog</a>
+        </li>
+        <li>
+          <a href="./contact">contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
---
+++ home.php
@@ -0,0 +1,74 @@
+<!--
+  Feature
+  ==========================================================================
+-->
+<div class="feature">
+  <div class="carousel slide box light container">
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="http://placehold.it/900x400">
+        <span class="tag dark lb">
+          <ol>
+            <li>
+              <a href="#">link 1</a>
+            </li>
+            <li>
+              <a href="#">link 2 long long long long</a>
+            </li>
+          </ol>
+        </span>
+        <span class="tag light rt">
+          <ul>
+            <li>
+              <a href="#">link 3</a>
+            </li>
+            <li>
+              <a href="#">link 4 long long long long</a>
+            </li>
+          </ul>
+        </span>
+      </div>
+    </div>
+    <a class="prev arrow" date-slide="prev" href="#myCarousel">‹</a>
+    <a class="next arrow" date-slide="next" href="#myCarousel">›</a>
+  </div>
+</div>
+<!--
+  Marketing
+  ==========================================================================
+-->
+<div class="marketing">
+  <ul class="thumbnails">
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+    <li class="span4">
+      <div class="thumbnail box light f">
+        <img src="http://placehold.it/284x200">
+      </div>
+    </li>
+  </ul>
+</div>
+<!-- /marketing -->
