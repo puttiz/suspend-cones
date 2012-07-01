ce69daf167c968e716cbc362496f41a24696b598
puttiz <puttiz@gmail.com>
2012-07-01 22:05:23 +0800

    + add site xml files

---
+++ lib/helpers/xml_site.rb
@@ -0,0 +1,63 @@
+module XMLSiteHelper
+
+	def xml_site(item)
+		require 'nokogiri'
+
+		output = Nokogiri::XML::Builder.new(:encoding => 'gbk') do |xml|
+			# site
+			xml.site {
+				item[:site].each do |name,value|
+					xml.send(name,value)
+				end
+				
+				# author
+				xml.author( :wangwang => item[:author][:wangwang], :url => item[:author][:url], :name => item[:author][:name] ) {
+					xml.text "#{item[:author][:name]}"
+				}
+
+				# global
+				xml.global {
+					item[:global].each do |n,v|
+						xml.send(n,v)
+					end
+				}
+
+				# themes
+				xml.themes( :default => item[:default_themes] ) {
+					item[:themes].each do |theme|
+						xml.theme( :name => theme[:name] ) {
+							xml.css theme[:css]
+							xml.rule theme[:rule]
+						}
+					end
+				}
+
+				# pages
+				xml.pages {
+					item[:pages].each do |p|
+						xml.page( :type => p[:type]) {
+							# TODO gbk
+							xml.name p[:name]
+							xml.file p[:file]
+							xml.thumbnail p[:thumbnail]
+							xml.description( "xsi:type" => "xs:string", "xmlns:xs" => "http://www.w3.org/2001/XMLSchema", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance" ) {
+								xml.text "#{p[:description]}"
+							}
+						}
+					end
+				}
+
+			}
+		end
+
+		# item[:author][:site_url]
+		# item[:themes][0][:name]
+
+		# def gbk
+		# 	dup.encode!("utf-8", "gbk")
+		# end
+
+		output.to_xml
+	end
+
+end
--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,2 +1,3 @@
 include ApplicationHelper
 include AttributeHelper
+include XMLSiteHelper
