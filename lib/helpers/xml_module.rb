931d7e75aa70c60fcb1585f0849b70ee6daa9e27
puttiz <puttiz@gmail.com>
2012-07-12 17:46:50 +0800

    + compile module files

--- lib/helpers/xml_module.rb
+++ lib/helpers/xml_module.rb
@@ -6,25 +6,36 @@ module XMLModuleHelper
 		output = Nokogiri::XML::Builder.new(:encoding => 'GBK') do |xml|
 			# module
 			xml.module {
-				item[:module].each do |name,value|
-					xml.send(name,value)
-				end
+				# item[:module].each do |name,value|
+				# 	xml.send(name,value)
+				# end
 
-				xml.description( "xsi:type" => "xs:string", "xmlns:xs" => "http://www.w3.org/2001/XMLSchema", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance" ) {
-						xml.text "#{item[:description]}"
-					}
-
+				xml.id item[:module][:id]
+				xml.name item[:module][:name]
 				xml.file {
 					xml.text "#{item[:module][:id]}.php"
 				}
-
+				xml.thumbnail "#{item[:module][:thumbnail]}"
+				xml.description( "xsi:type" => "xs:string", "xmlns:xs" => "http://www.w3.org/2001/XMLSchema", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance" ) {
+						xml.text "#{item[:module][:description]}"
+					}
+				xml.requiredCache item[:module][:requiredCache]
+				xml.supportedWidth item[:module][:supportedWidth]
+				
 				# themes
-				xml.themes( :default => item[:default_themes] ) {
+				xml.themes( :default => item[:module][:theme] ) {
 					item[:themes].each do |theme|
 						xml.theme( :name => theme[:name] ) {
-							xml.css theme[:css]
+							xml.css "assets/stylesheets/#{theme[:name]}.css"
 						}
 					end
+				}
+
+				# parameters
+				xml.parameters {
+					# item[:parameters].each do |p|
+					# 	#
+					# end
 				}
 
 			}
