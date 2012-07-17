a968ab20da56ced47a13920adb3327bc684176c2
puttiz <puttiz@gmail.com>
2012-07-17 17:24:08 +0800

    + livereload :apply_css_live => false + widget carousel + add add_import_path and sprite_load_path for compass

--- config.rb
+++ config.rb
@@ -1,12 +1,11 @@
 require 'compass_twitter_bootstrap'
 
 project_path = File.dirname(__FILE__)
-http_path    = '/'
+http_path    = './'
 output_style = :expanded
-sass_dir     = 'content/assets/stylesheets'
-css_dir      = 'output/assets/stylesheets'
-images_dir   = 'output/assets/images'
+# sass_dir     = 'content/assets/stylesheets'
+# css_dir      = 'output/assets/stylesheets'
+images_dir   = 'assets/images'
+sprite_load_path = 'lib/sprites'
 
-# sass_option = {
-# 	:syntax => :scss
-# }
+add_import_path 'lib/stylesheets'
--- lib/helpers/attribute.rb
+++ lib/helpers/attribute.rb
@@ -18,7 +18,7 @@ module AttributeHelper
 	def carousel
 		{
 			:'data-widget-type' => "Carousel",
-			:'data-widget-config' => "{'contentCls':'carousel-inner', 'navCls':'carousel-switch', 'activeTriggerCls':'active', 'prevBtnCls':'left', 'nextBtnCls':'right', 'effect':'<?=$_MODULE['effect']?>', 'easing':'easeBoth', 'triggerType':'click', 'duration':'1.2'}"
+			:'data-widget-config' => "{'contentCls':'carousel-items', 'navCls':'carousel-switch', 'activeTriggerCls':'active', 'prevBtnCls':'left', 'nextBtnCls':'right', 'effect':'<?=$_MODULE['effect']?>', 'easing':'easeBoth', 'triggerType':'click', 'duration':'1.2'}"
 		}
 	end
 end
